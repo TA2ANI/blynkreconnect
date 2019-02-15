bc086a3b0ab5f9242d605bde858e10ab37ca85e3
TA2ANI <44003703+TA2ANI@users.noreply.github.com>
2019-02-15 22:22:52 +0300

    Add files via upload

---
+++ BLYNK_RECONNECT.ino
@@ -0,0 +1,83 @@
+
+#define BLYNK_PRINT Serial
+#define sure 20
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+BlynkTimer timer;
+
+
+
+char auth[] = "..................."; // BLYNK TOKEN
+
+
+
+
+char ssid[] = "................";     // WiFi adı
+char pass[] = "................";     // WiFi şifresi
+
+
+
+bool wifidurum = true;
+
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  WiFi.begin (ssid, pass);
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(1000L, uyari);
+  timer.setInterval(100L, kontrol);
+}
+
+
+
+
+
+void uyari()
+{
+
+  if (wifidurum)
+  {
+    // KENDİ İSTEDİĞİM İŞLEMLERİ YAPTIRDIĞIM YER
+  }
+  
+}
+
+
+
+void loop()
+{
+  if (Blynk.connected())
+  {
+    Blynk.run();
+    timer.run();
+    uyari ();
+  }
+
+  else
+  {
+    wifidurum = false;
+    kontrol ();
+  }
+}
+
+
+
+
+void kontrol()
+{
+  if (!wifidurum)
+  {
+    while ( WiFi.status () != WL_CONNECTED )
+    {
+      Serial.println ("baglanti koptu");
+      delay (500);
+    }
+    WiFi.begin (ssid, pass);
+    Blynk.begin(auth, ssid, pass);
+  }
+  wifidurum = true;
+}
